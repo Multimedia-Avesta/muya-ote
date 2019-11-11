a194171d62b53176dbbcd4246a9345a4e07b53ef
Martin Sievers <sievers@uni-trier.de>
2019-11-11 10:40:37 +0100

    Edit changelog.htm

--- wce-ote/plugin/plugin.js
+++ wce-ote/plugin/plugin.js
@@ -33,7 +33,7 @@
 */
 
 (function() {
-   var wfce_editor = "1.4.2 (2019-11-08)";
+   var wfce_editor = "1.4.2 (2019-11-11)";
 
    // Load plugin specific language pack
    tinymce.PluginManager.requireLangPack('wce');
