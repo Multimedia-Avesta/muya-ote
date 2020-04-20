133132332d36a81aad7b928849ce25ee7c3bef9d
Martin Sievers <sievers@uni-trier.de>
2020-04-20 21:19:26 +0200

    Change zip script

--- make_newversion.sh
+++ make_newversion.sh
@@ -1,2 +1,3 @@
 FILE1=$1
-zip -u -r -9 -v  muya_$FILE1.zip COPYING COPYING.LESSER LICENSE.TXT README changelog.txt js/ tools/ wce-ote/
+zip -u -r -9 -v  muya_$FILE1.zip COPYING COPYING.LESSER LICENSE.TXT README changelog.txt js/ tools/ wce-ote/ -x "*~"
+
