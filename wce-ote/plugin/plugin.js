949349d3a579b0440d17ba3a65f4ad88a08cb31b
Martin Sievers <sievers@uni-trier.de>
2020-04-21 11:34:06 +0200

    Prepare release

--- wce-ote/plugin/plugin.js
+++ wce-ote/plugin/plugin.js
@@ -33,7 +33,7 @@
 */
 
 (function() {
-   var wfce_editor = "1.5.1BETA (2020-04-20)";
+   var wfce_editor = "1.5.1 (2020-04-21)";
 
    // Load plugin specific language pack
    tinymce.PluginManager.requireLangPack('wce');
